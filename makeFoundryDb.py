45213dabe0167f007446875810e87b2f45dad4e4
Eric Stevens <eric@ericstevens.ca>
2023-04-15 13:01:05 -0400

    added armor processing

--- makeFoundryDb.py
+++ makeFoundryDb.py
@@ -2,6 +2,87 @@ import csv
 import json
 from pathlib import Path
 
+def makeOutputRecord(csvData:dict, type:str)->dict:
+    if type == 'armor':
+        # process as armor
+        outputRecord = {
+            "name": "",
+            "type": "armor",
+            "img": "",
+            "system": {
+                "name": "",
+                "techLevel": 0,
+                "description": "Armor",
+                "shortdescr": "",
+                "quantity": 1,
+                "weight": 1,
+                "price": 0,
+                "traits": [],
+                "consumables": [],
+                "skillModifier": 0,
+                "skill": "",
+                "associatedSkillName": "",
+                "equipped": "",
+                "docReference": "",
+                "pdfReference": {
+                    "type": "",
+                    "href": "",
+                    "label": ""
+                },
+                "armor": 0,
+                "secondaryArmor": {
+                    "value": 0,
+                    "protectionTypes": []
+                },
+                "radiationProtection": {
+                    "value": 0
+                },
+                "type": "armor",
+                "useConsumableForAttack": "",
+                "isPowered": false
+            },
+            "effects": [],
+            "flags": {
+                "twodsix": {
+                    "newItem": true
+                },
+                "core": {
+                    "sourceId": "Item.kHeev9TUmoxCl1uD"
+                }
+            },
+            "_stats": {
+                "systemId": "twodsix",
+                "systemVersion": "2.27.3",
+                "coreVersion": "10.291",
+                "createdTime": 1681494327937,
+                "modifiedTime": 1681494395684,
+                "lastModifiedBy": "OK9YthkIIxCpCkT0"
+            },
+            "folder": null,
+            "sort": 0,
+            "ownership": {
+                "default": 0,
+                "OK9YthkIIxCpCkT0": 3
+            },
+            "_id": "bmMpPYi1MEHTx7mh"
+        }
+        outputRecord['img'] = "icons/svg/item-bag.svg"
+        outputRecord['name'] = csvData['name']
+        outputRecord['system']['techLevel'] = csvData['techlevel']
+        outputRecord['system']['quantity'] = csvData['quantity']
+        outputRecord['system']['wieght'] = csvData['weight']
+        outputRecord['system']['price'] = csvData['price']
+        outputRecord['system']['armor'] = csvData['armor']
+        outputRecord['system']['secondaryArmor']['value'] = csvData['secondary']
+        outputRecord['system']['radiationProtection']['value'] = csvData['radiation']
+        for t in csvData['secondarytypes'].split(":"):
+            outputRecord['system']['secondaryArmor']['protectionTypes'].append(t)
+        return outputRecord
+    else:
+        print(f"{type} type not yet supported")
+        return {}
+
+
 def main ():
     inputFolder = "input"
     outputFolder = "output"
@@ -20,10 +101,11 @@ def main ():
         if f.name == armorFile+inputSuffix:
             # process as armor
             print(f"processing armor file {f.name}")
-            with open(f, newline='') as csvfile:
-                csvInput = csv.reader(csvfile, delimiter=' ', quotechar='"')
-                for row in csvInput:
-                    print(', '.join(row))
+            with open(Path(outputFolder, foundryPrefix+armorFile+outputSuffix), 'w') as dbFile:
+                with open(f, newline='') as csvfile:
+                    csvInput = csv.DictReader(csvfile)
+                    for row in csvInput:
+                        dbFile.write(makeOutputRecord(row, 'armor'))
         if f.name == weaponFile+inputSuffix:
             # process as wepon
             print(f"processing weapon file {f.name}")
