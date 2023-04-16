3f0f7bc2474ba00a7fcfa07f6dbf1748d83913f1
swdevlin <swdevlin@gmail.com>
2023-04-16 18:01:09 -0400

    Armor parser

---
+++ armor_parser.py
@@ -0,0 +1,28 @@
+import re
+
+
+def parse_secondary(text):
+    text = text.strip()
+    if 'PSI' in text:
+        tokens = [text.replace(' PSI', ''), 'PSI']
+    else:
+        tokens = text.split(' vs. ')
+    tokens[0] = tokens[0].replace('+','')
+    tokens = list(s.strip() for s in tokens)
+    return tokens[0], tokens[1]
+
+
+def parse_armor(line):
+    plus_split = line.split('+', 1)
+    name = plus_split[0]
+    specs = plus_split[1]
+    secondary_protection = ''
+    secondary_protection_type = ''
+    if '(' in specs:
+        secondary = re.search(r'\((.*)\)', specs)
+        secondary = secondary.group(1)
+        specs = specs.replace(f'({secondary}) ', '')
+        secondary_protection, secondary_protection_type = parse_secondary(secondary)
+    tokens = specs.split(' ')
+    tokens = tokens[:1] + [secondary_protection, secondary_protection_type] + tokens[1:]
+    return [name.strip()] + tokens
---
+++ tests/test_armor.py
@@ -0,0 +1,66 @@
+import unittest
+
+from armor_parser import parse_armor, parse_secondary
+
+
+class TestArmorParser(unittest.TestCase):
+    def test_standard(self):
+        line = 'Lightweight Poly Carapace +12 11 - 2 Cr15000 None'
+        expected = [
+            'Lightweight Poly Carapace',
+            '12',
+            '',
+            '',
+            '11',
+            '-',
+            '2',
+            'Cr15000',
+            'None'
+        ]
+        tokens = parse_armor(line)
+        self.assertEqual(expected, tokens)
+
+    def test_alternate(self):
+        line = 'Ablat +1 (+6 vs. lasers) 9 - 2 Cr75 None'
+        expected = [
+            'Ablat',
+            '1',
+            '6',
+            'lasers',
+            '9',
+            '-',
+            '2',
+            'Cr75',
+            'None'
+        ]
+        tokens = parse_armor(line)
+        self.assertEqual(expected, tokens)
+
+    def test_psi(self):
+        line = 'Psi-Enhanced Combat Armour +15 (+ ½ PSI) 16 250 10 Cr500000 None'
+        expected = [
+            'Psi-Enhanced Combat Armour',
+            '15',
+            '½',
+            'PSI',
+            '16',
+            '250',
+            '10',
+            'Cr500000',
+            'None'
+        ]
+        tokens = parse_armor(line)
+        self.assertEqual(expected, tokens)
+
+    def test_secondary(self):
+        line = '+6 vs. lasers'
+        expected = (
+            '6',
+            'lasers',
+        )
+        tokens = parse_secondary(line)
+        self.assertEqual(expected, tokens)
+
+
+if __name__ == '__main__':
+    unittest.main()
